c889b097584e0180410be4986b602e5082074bc8
GitHub Action <action@github.com>
2022-12-18 13:50:26 +0000

    Github Action Build of 18d5830

--- lib/libpng/libpng16-relwithdebinfo.cmake
+++ lib/libpng/libpng16-relwithdebinfo.cmake
@@ -12,8 +12,8 @@ set_target_properties(png_static PROPERTIES
   IMPORTED_LOCATION_RELWITHDEBINFO "${_IMPORT_PREFIX}/lib/libpng16.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS png_static )
-list(APPEND _IMPORT_CHECK_FILES_FOR_png_static "${_IMPORT_PREFIX}/lib/libpng16.a" )
+list(APPEND _cmake_import_check_targets png_static )
+list(APPEND _cmake_import_check_files_for_png_static "${_IMPORT_PREFIX}/lib/libpng16.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
