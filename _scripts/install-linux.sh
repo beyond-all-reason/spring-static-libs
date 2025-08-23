3bad19a7775e85294cfa68f9716cdc65495e93ad
Marek Rusinowski <marekrusinowski@gmail.com>
2025-08-23 18:03:41 +0200

    Simplify install-linux
    - Remove some no longer needed packages
    - Drop no longer used apt download

--- _scripts/install-linux.sh
+++ _scripts/install-linux.sh
@@ -5,70 +5,49 @@ source $(dirname $0)/make_static_libs_common.sh
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt update -y
-apt -y install make ninja-build \
-	p7zip-full \
-	freeglut3-dev \
-	libogg-dev \
-	wget \
-	vim \
-	dpkg-dev \
-	apt-utils \
-	pkg-config \
-	po4a \
-	autoconf \
-	libtool \
-	autopoint \
-	git \
-	xmlto \
-	python3-pip
+case $UBUNTU_MAJORVER in
+    20) GCCVER="13" ;;
+    18) GCCVER="13" ;;
+    *)
+        echo "Unsupported ubuntu version $UBUNTU_MAJORVER"
+        exit 1
+        ;;
+esac
+
+apt-get update
+apt-get install --no-install-recommends --yes software-properties-common
+add-apt-repository ppa:ubuntu-toolchain-r/test --yes
+apt-get update
+apt-get upgrade --yes
+
+# Base build dependencies:
+apt-get install --no-install-recommends --yes \
+    make \
+    wget \
+    pkg-config \
+    git \
+    python3-pip \
+    gcc-$GCCVER \
+    g++-$GCCVER
+
+# For GLEW:
+apt-get install --no-install-recommends --yes libxmu-dev libxi-dev libgl-dev
 
 pip3 install --upgrade pip
 pip3 install scikit-build
 pip3 install cmake==3.27.*
 
-apt install -y build-essential software-properties-common
-
-apt remove -y snapd || true
-apt remove -y gcc g++ || true
-apt remove -y $(apt list --installed | cut -d'/' -f1 | grep -e "gcc-[0-9]+$")
-apt remove -y $(apt list --installed | cut -d'/' -f1 | grep -e "g\+\+-[0-9]+*$")
-
-#apt install -y gcc-snapshot
-case $UBUNTU_MAJORVER in
-20)
-  VER="13"
-  add-apt-repository ppa:ubuntu-toolchain-r/test -y
-  apt update -y
-  ;;
-18)
-  VER="13"
-  add-apt-repository ppa:ubuntu-toolchain-r/test -y
-  apt update -y
-  ;;
-*)
-  exit 1
-  ;;
-esac
-
-apt install -y gcc-$VER g++-$VER
-
-apt -y autoremove
+update-alternatives \
+--install /usr/bin/gcc gcc /usr/bin/gcc-$GCCVER 60 \
+--slave /usr/bin/g++ g++ /usr/bin/g++-$GCCVER \
+--slave /usr/bin/gcov gcov /usr/bin/gcov-$GCCVER \
+--slave /usr/bin/gcov-tool gcov-tool /usr/bin/gcov-tool-$GCCVER \
+--slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-$GCCVER \
+--slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-$GCCVER \
+--slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-$GCCVER
 
 update-alternatives \
---install /usr/bin/gcc gcc /usr/bin/gcc-$VER 60 \
---slave /usr/bin/g++ g++ /usr/bin/g++-$VER \
---slave /usr/bin/gcov gcov /usr/bin/gcov-$VER \
---slave /usr/bin/gcov-tool gcov-tool /usr/bin/gcov-tool-$VER \
---slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-$VER \
---slave /usr/bin/gcc-nm gcc-nm /usr/bin/gcc-nm-$VER \
---slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-$VER
+--install /usr/bin/c++ c++ /usr/bin/g++-$GCCVER 60
 
 update-alternatives \
---install /usr/bin/c++ c++ /usr/bin/g++-$VER 60
-
-update-alternatives \
---install /usr/bin/cc cc /usr/bin/gcc-$VER 60
-
-sed -i 's/# deb-src/deb-src/g' /etc/apt/sources.list
-apt update -y
+--install /usr/bin/cc cc /usr/bin/gcc-$GCCVER 60
